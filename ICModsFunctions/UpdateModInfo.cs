687203d21ad51192dddb8e0aded2e1c1c366ecf3
Nikolay Savenko <savenkons@yandex.ru>
2021-01-01 23:44:49 +0300

    Finished UpdateModInfo

--- ICModsFunctions/UpdateModInfo.cs
+++ ICModsFunctions/UpdateModInfo.cs
@@ -8,33 +8,63 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.Net;
+using System.Data.SqlClient;
+using System.Text;
 
 namespace ICModsFunctions
 {
     public static class UpdateModInfo
     {
         static readonly string descriptionAPI = "https://icmods.mineprogramming.org/api/description";
+        static readonly string tableName = "mods_downloads";
         [FunctionName("UpdateModInfo")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-
-            ILogger log)
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req, ILogger log)
         {
+            string responseMessage;
             string modID = req.Query["id"];
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            modID = modID ?? data?.name;
+            string statID = req.Query["statID"];
+            log.LogInformation(req.Query["statID"]);
+            string downloads = getDownloadsForMod(modID, log);
 
-            int? downloads = getDownloadsForMod(modID, log);
+            if (!String.IsNullOrEmpty(downloads))
+            {
+                log.LogInformation($"try updateTable()");
+                await updateTableAsync(statID, modID, downloads, log);
+                log.LogInformation($"after updateTable()");
+                responseMessage = "0";
+            } else responseMessage = "1";
+            
+            
+            
 
-            string responseMessage = downloads == null
-                ? "Fail!"
-                : $"Mod {modID}\nDownloads: {downloads}";
-            // new BadRequestObjectResult()
             return new OkObjectResult(responseMessage);
         }
 
-        public static int? getDownloadsForMod(string modID, ILogger log)
+        public static async Task<int> updateTableAsync(string statID, string modID, string downloads, ILogger log) {
+            log.LogInformation($"updateTable()");
+            var str = Environment.GetEnvironmentVariable("sqldb_connection");
+
+            using (SqlConnection conn = new SqlConnection(str))
+            {
+                conn.Open();
+                var sb = new StringBuilder();
+                sb.AppendLine($"UPDATE {tableName} SET modID_{modID} = {downloads} where stat_id = {statID}");
+                
+                log.LogInformation($"sqlCommand id {sb}");
+                using (SqlCommand cmd = new SqlCommand(sb.ToString(), conn))
+                {
+                    // Execute the command and log the # rows affected.
+                    var rows = await cmd.ExecuteNonQueryAsync();
+                    log.LogInformation($"{rows} rows were updated");
+                    
+                }
+            }
+            log.LogInformation($"`finished updateTable()");
+            return 0;
+        }
+
+        public static string getDownloadsForMod(string modID, ILogger log)
         {
             log.LogInformation($"Getting downloads for: {modID}");
             dynamic description = JsonConvert.DeserializeObject(getModDescription(modID, log));
