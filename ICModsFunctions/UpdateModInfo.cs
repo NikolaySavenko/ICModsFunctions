67cc12b158ccbc35a567764b87989b521d0c6cda
Nikolay Savenko <savenkons@yandex.ru>
2021-01-02 17:37:32 +0300

    ActivityTrigger for UpdateModInfo

--- ICModsFunctions/UpdateModInfo.cs
+++ ICModsFunctions/UpdateModInfo.cs
@@ -10,6 +10,8 @@ using Newtonsoft.Json;
 using System.Net;
 using System.Data.SqlClient;
 using System.Text;
+using System.Net.Http;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 
 namespace ICModsFunctions
 {
@@ -18,31 +20,20 @@ namespace ICModsFunctions
         static readonly string descriptionAPI = "https://icmods.mineprogramming.org/api/description";
         static readonly string tableName = "mods_downloads";
         [FunctionName("UpdateModInfo")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req, ILogger log)
+        public static int Run([ActivityTrigger] string input, ILogger log)
         {
-            string responseMessage;
-            string modID = req.Query["id"];
-            string statID = req.Query["statID"];
-            log.LogInformation(req.Query["statID"]);
-            string downloads = getDownloadsForMod(modID, log);
+            string[] data = input.Split(",");
+            string downloads = getDownloadsForMod(data[1], log);
 
             if (!String.IsNullOrEmpty(downloads))
             {
-                log.LogInformation($"try updateTable()");
-                await updateTableAsync(statID, modID, downloads, log);
-                log.LogInformation($"after updateTable()");
-                responseMessage = "0";
-            } else responseMessage = "1";
-            
-            
-            
+                Task.Run(() => updateTableAsync(data[0], data[1], downloads, log)).Wait();
+            } 
 
-            return new OkObjectResult(responseMessage);
+            return 0;
         }
 
         public static async Task<int> updateTableAsync(string statID, string modID, string downloads, ILogger log) {
-            log.LogInformation($"updateTable()");
             var str = Environment.GetEnvironmentVariable("sqldb_connection");
 
             using (SqlConnection conn = new SqlConnection(str))
@@ -51,31 +42,23 @@ namespace ICModsFunctions
                 var sb = new StringBuilder();
                 sb.AppendLine($"UPDATE {tableName} SET modID_{modID} = {downloads} where stat_id = {statID}");
                 
-                log.LogInformation($"sqlCommand id {sb}");
                 using (SqlCommand cmd = new SqlCommand(sb.ToString(), conn))
                 {
-                    // Execute the command and log the # rows affected.
-                    var rows = await cmd.ExecuteNonQueryAsync();
-                    log.LogInformation($"{rows} rows were updated");
-                    
+                    await cmd.ExecuteNonQueryAsync();
                 }
             }
-            log.LogInformation($"`finished updateTable()");
             return 0;
         }
 
         public static string getDownloadsForMod(string modID, ILogger log)
         {
-            log.LogInformation($"Getting downloads for: {modID}");
             dynamic description = JsonConvert.DeserializeObject(getModDescription(modID, log));
             return description.downloads;
         }
 
         public static string getModDescription(string modID, ILogger log)
         {
-            log.LogInformation($"Getting description for: {modID}");
             string requestURI = getRequestById(modID);
-            log.LogInformation($"URI {requestURI}");
             var request = WebRequest.Create(requestURI);
             request.Credentials = CredentialCache.DefaultCredentials;
 
