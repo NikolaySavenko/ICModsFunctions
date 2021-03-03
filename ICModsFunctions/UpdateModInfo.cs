d7f271f44a78d55cf0444c5f94c702ac9a8a38f2
Nikolay Savenko <savenkons@yandex.ru>
2021-03-03 22:49:34 +0300

    try to move on Entity Framework

--- ICModsFunctions/UpdateModInfo.cs
+++ ICModsFunctions/UpdateModInfo.cs
@@ -1,53 +1,41 @@
 using System;
-using System.IO;
-using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using System.Net;
-using System.Data.SqlClient;
-using System.Text;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using ICModsFunctions.Model;
 
 namespace ICModsFunctions
 {
-    public static class UpdateModInfo
-    {
-        static readonly string tableName = "mods_downloads";
+	public static class UpdateModInfo
+	{
+		[FunctionName("UpdateModInfo")]
+		public static int Run([ActivityTrigger] string modID, ILogger log)
+		{
+			if (Int32.TryParse(modID, out int downloadsCount))
+			{
+				string downloads = Mineprogramming.GetDownloads(modID);
+				if (String.IsNullOrEmpty(downloads)) return 0;
 
-        [FunctionName("UpdateModInfo")]
-        public static int Run([ActivityTrigger] string modID, ILogger log)
-        {
-            if (Int32.TryParse(modID, out int downloadsCount)) {
-                string downloads = Mineprogramming.GetDownloads(modID);
-                if (String.IsNullOrEmpty(downloads)) return 0;
+				MakeStatEntry(modID, downloads, log);
+				return 1;
+			}
+			return 0;
+		}
 
-                makeStatEntry(modID, downloads, log);
-                return 1;
-            }
-            return 0;
-        }
-
-        public static string makeStatEntry(string modID, string downloads, ILogger log)
-        {
-            var str = Environment.GetEnvironmentVariable("sqldb_connection");
-            string newStatID = String.Empty;
-            using (SqlConnection conn = new SqlConnection(str))
-            {
-                conn.Open();
-                var sb = new StringBuilder();
-                sb.AppendLine($"INSERT into {tableName}(stat_time, mod_id, downloads)");
-                sb.AppendLine("VALUES");
-                sb.AppendLine($"(CONVERT(smalldatetime, GETDATE()), {modID}, {downloads})");
-                // sb.AppendLine("SELECT SCOPE_IDENTITY()");
-
-                using (SqlCommand cmd = new SqlCommand(sb.ToString(), conn))
-                {
-                    Task.Run(() => cmd.ExecuteNonQuery()).Wait();
-                }
-
-            }
-            return newStatID;
-        }
-    }
+		public static string MakeStatEntry(string modID, string downloads, ILogger log)
+		{
+			using (var dbContext = new ICModsStatisticsDatabaseContext())
+			{
+				dbContext.ModsDownloadsTest.Add(new ModsDownloadsTest
+				{
+					StatTime = DateTime.Now,
+					Downloads = Int32.Parse(downloads),
+					ModId = Int32.Parse(modID)
+				});
+				log.LogInformation($"invoked for {modID}");
+				dbContext.SaveChanges();
+			}
+			return "";
+		}
+	}
 }
