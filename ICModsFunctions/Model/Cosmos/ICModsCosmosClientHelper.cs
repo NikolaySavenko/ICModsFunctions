b0fa40af296ac145f8b1f3f8ea2bbfc246372cd7
Savenko Nikolay Sergeevich <Nikolay.Savenko@geropharm.com>
2021-09-21 12:54:31 +0300

    simple cosmos model

---
+++ ICModsFunctions/Model/Cosmos/ICModsCosmosClientHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Azure.Cosmos;
+
+namespace ICModsFunctions.Model.Cosmos
+{
+    internal class ICModsCosmosClientHelper
+    {
+        // The Azure Cosmos DB endpoint for running this sample.
+        private static readonly string EndpointUri = Environment.GetEnvironmentVariable("EndPointUri");
+
+        // The primary key for the Azure Cosmos account.
+        private static readonly string PrimaryKey = Environment.GetEnvironmentVariable("PrimaryKey");
+
+        // The Cosmos client instance
+        private CosmosClient cosmosClient;
+
+        // The database we will create
+        private Database database;
+
+        // The container we will create.
+        private Container container;
+
+        // The name of the database and container we will create
+        private string databaseId = Environment.GetEnvironmentVariable("DatabaseId");
+        private string containerId = Environment.GetEnvironmentVariable("ContainerId");
+
+    }
+}
---
+++ ICModsFunctions/Model/Cosmos/ModStatItem.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace ICModsFunctions.Model.Cosmos
+{
+    internal class ModStatItem
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+        [JsonProperty(PropertyName = "partitionKey")]
+        public string PartitionKey { get; set; }
+        public int ModId { get; set; }
+        public DateTime StatTime { get; set; }
+        public int DownloadsCount { get; set; }
+    }
+}
