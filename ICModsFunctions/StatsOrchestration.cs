b90cea5327474c8cfb6ecbe6ef4a2ee659215ff8
Nikolay Savenko <savenkons@yandex.ru>
2022-02-19 19:32:14 +0300

    refactor stats orchestration

--- ICModsFunctions/StatsOrchestration.cs
+++ ICModsFunctions/StatsOrchestration.cs
@@ -4,56 +4,65 @@ using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using System;
 using Newtonsoft.Json;
-using ApplicationCore;
 using ApplicationCore.Model.InnerCore;
+using System.Linq;
 
 namespace ICModsFunctions
 {
 	public static class StatsOrchestration
 	{
-
-		[FunctionName(nameof(RunStatisticsOrchestrator))]
-		public static async Task<List<InnerCoreModDescription>> RunStatisticsOrchestrator(
-			[OrchestrationTrigger] IDurableOrchestrationContext context, ILogger log)
-		{
-			Uri uri = new Uri("https://icmods.mineprogramming.org/api/list?start=0&count=10000&horizon");
-			var listPesponse = await context.CallHttpAsync(HttpMethod.Get, uri);
-			List<InnerCoreModData> modsList = JsonConvert.DeserializeObject<List<InnerCoreModData>>(listPesponse.Content);
-			var parallelsTasks = new List<Task<DurableHttpResponse>>();
-			foreach (var modData in modsList)
-			{
-				Uri descriptionUri = new Uri($"https://icmods.mineprogramming.org/api/description?id={modData.Id}");
-				parallelsTasks.Add(context.CallHttpAsync(HttpMethod.Get, descriptionUri));
-			}
-			var responses = await Task.WhenAll(parallelsTasks);
-			var modDescriptions = new List<InnerCoreModDescription>();
-			var dbWrites = new List<Task>();
-			foreach(var response in responses)
-			{
-				var description = JsonConvert.DeserializeObject<InnerCoreModDescription>(response.Content);
-				modDescriptions.Add(description);
-				dbWrites.Add(context.CallActivityAsync(nameof(CosmosWriter.WriteCosmosStat), description));
-			}
-			await Task.WhenAll(dbWrites);
-			return modDescriptions;
-		}
-
-
-		[FunctionName("StatsOrchestration_HttpStart")]
-		public static async Task<HttpResponseMessage> HttpStart(
+		[FunctionName(nameof(StartOrchestrationByHttp))]
+		public static async Task<HttpResponseMessage> StartOrchestrationByHttp(
 			[HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestMessage req,
 			[DurableClient] IDurableOrchestrationClient starter,
 			ILogger log)
 		{
-			// Function input comes from the request content.
 			string instanceId = await starter.StartNewAsync(nameof(RunStatisticsOrchestrator), null);
-
 			log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
 			return starter.CreateCheckStatusResponse(req, instanceId);
+		}
+
+		[FunctionName(nameof(RunStatisticsOrchestrator))]
+		public static async Task RunStatisticsOrchestrator(
+			[OrchestrationTrigger] IDurableOrchestrationContext context, ILogger log)
+		{
+			var descriptions = await GetModsDescriptions(context);
+			await WriteModsDescriptions(descriptions, context);
+		}
+
+		private static async Task<IEnumerable<InnerCoreModDescription>> GetModsDescriptions(IDurableOrchestrationContext context)
+		{
+			var mods = await GetModsContent<InnerCoreModDescription>(context);
+			var requests = mods.Select(mod => context.CallHttpAsync(HttpMethod.Get, GetDescriptionUri(mod.Id)));
+
+			var responses = await Task.WhenAll(requests);
+			var descriptions = responses.Select(r => JsonConvert.DeserializeObject<InnerCoreModDescription>(r.Content));
+
+			return descriptions;
+		}
+
+		private static async Task WriteModsDescriptions<T>(IEnumerable<T> descriptions, IDurableOrchestrationContext context)
+		{
+			var writeFunctionName = nameof(CosmosWriter.WriteCosmosStat);
+			var activities = descriptions.Select(d => context.CallActivityAsync(writeFunctionName, d));
+
+			await Task.WhenAll(activities);
+		}
+
+		private static async Task<TModContent[]> GetModsContent<TModContent>(IDurableOrchestrationContext context)
+		{
+			var apiEndpoint = new Uri("https://icmods.mineprogramming.org/api/list?start=0&count=10000&horizon");
+			var apiCall = await context.CallHttpAsync(HttpMethod.Get, apiEndpoint);
+			var mods = JsonConvert.DeserializeObject<TModContent[]>(apiCall.Content);
+			return mods;
+		}
+
+		private static Uri GetDescriptionUri(int modId)
+		{
+			return new Uri($"https://icmods.mineprogramming.org/api/description?id={modId}");
 		}
 	}
 }
